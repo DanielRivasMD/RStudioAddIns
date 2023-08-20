b8a1ec170027f48437099445133d94c93ae0451c
DanielRivasMD <danielrivasmd@gmail.com>
2023-08-20 11:06:28 -0600

    patch double colon

--- R/InsertDoubleColon.R
+++ R/InsertDoubleColon.R
@@ -1,10 +1,10 @@
 #' Insert ::.
 #'
-#' Call this function as an addin to insert \code{ :: } at the cursor position.
+#' Call this function as an addin to insert \code{::} at the cursor position.
 #'
 #' @importFrom rstudioapi insertText
 #'
 #' @export
 InsertDoubleColon <- function() {
-  rstudioapi::insertText(" :: ")
+  rstudioapi::insertText("::")
 }
